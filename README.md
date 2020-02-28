76d5212bbe21cdfca329d73ebd4a931c3cf3a90c
Tom Witkowski <dev.gummibeer@gmail.com>
2020-02-28 23:48:53 +0100

    add unittests

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
 
 ### Security
 
-If you discover any security related issues, please email gummibeer@astrotomic.info instead of using the issue tracker.
+If you discover any security related issues, please email dev@astrotomic.info instead of using the issue tracker.
 
 ## Credits
 
--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -2,6 +2,7 @@
 
 namespace Astrotomic\PhpWeservImages\Tests;
 
+use Astrotomic\Weserv\Images\Enums\Fit;
 use Astrotomic\Weserv\Images\Url;
 use PHPUnit\Framework\TestCase;
 
@@ -14,5 +15,24 @@ final class UrlTest extends TestCase
 
         static::assertInstanceOf(Url::class, $url);
         static::assertSame('https://images.weserv.nl?url=https%3A%2F%2Fexample.com%2Fimage.jpg', (string) $url);
+    }
+
+    /** @test */
+    public function it_can_build_with_custom_base_url(): void
+    {
+        $url = new Url('https://example.com/image.jpg', 'https://images.astrotomic.info');
+
+        static::assertInstanceOf(Url::class, $url);
+        static::assertSame('https://images.astrotomic.info?url=https%3A%2F%2Fexample.com%2Fimage.jpg', (string) $url);
+    }
+
+    /** @test */
+    public function it_can_build_with_custom_dimensions(): void
+    {
+        $url = new Url('https://example.com/image.jpg');
+        $url->w(1920)->h(1080)->fit(Fit::CONTAIN)->we();
+
+        static::assertInstanceOf(Url::class, $url);
+        static::assertSame('https://images.weserv.nl?w=1920&h=1080&fit=contain&we=1&url=https%3A%2F%2Fexample.com%2Fimage.jpg', (string) $url);
     }
 }
